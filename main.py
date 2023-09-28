344f4614ad5be7647e2557b5c80ea4fbec484d8b
Chris Ditfort <chrisditfort@outlook.com>
2023-09-28 23:11:17 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from tkinter import ttk
+
+#Window
+window = tk.Tk()
+window.title('Tkinter Variables')
+
+#tkinter variable
+string_var = tk.StringVar()
+
+#Widgets
+label = ttk.Label(master=window,text='Label', textvariable=string_var)
+label.pack()
+
+entry = ttk.Entry(master=window, textvariable=string_var)
+entry.pack()
+
+entry2 = ttk.Entry(master=window, textvariable=string_var)
+entry2.pack()
+
+#Run
+window.mainloop()
+
